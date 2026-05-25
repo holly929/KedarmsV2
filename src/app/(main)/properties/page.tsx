f448e0a98fee9f3e2010b09dbaf6f5553e3370e3
Aneh Master <masteraneh247@gmail.com>
2026-05-25 07:16:40 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: cn is n

--- src/app/(main)/properties/page.tsx
+++ src/app/(main)/properties/page.tsx
@@ -15,6 +15,7 @@ import {
 } from 'lucide-react';
 import * as XLSX from 'xlsx';
 
+import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import {
